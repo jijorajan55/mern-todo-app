a221ab9e485fd9f3e963849b37018550ea9c3b5f
Jo <jijorajan55@gmail.com>
2024-12-12 12:48:37 +0530

    Update server.js

--- mern-todo-app/server.js
+++ mern-todo-app/server.js
@@ -12,11 +12,21 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 // Routes
 app.use('/api/todos', require('./routes/todoRoutes'));
+
+// Default route to handle "Cannot GET" for undefined routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Todo App API');
+});
+
+// Handle undefined routes
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
